56104be3069196d7832c0ea9845a4eade56d2d42
ANU-Chetan <u7798996@anu.edu.au>
2026-01-07 00:31:51 +1100

    fixinghy tailwindcss issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Chetan-Battan.github.io',
-  base: '/chetanbattan-portfolio'
+  base: '/chetanbattan-portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
